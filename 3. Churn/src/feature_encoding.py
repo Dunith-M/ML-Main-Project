073653045c86eb812330d48b15a60d1a16bb3f63
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-12-30 15:20:04 +0530

    update

---
+++ 3. Churn/src/feature_encoding.py
@@ -0,0 +1,22 @@
+import logging
+import pandas as pd
+import os
+import json
+from enum import Enum
+from typing import Dict, List
+from abc import ABC, abstractmethod
+logging.basicConfig(level=logging.INFO, format=
+    '%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+class FeatureEncodingStrategy(ABC):
+    @abstractmethod
+    def encode(self, df: pd.DataFrame) ->pd.DataFrame:
+        pass
+
+
+class VariableType(str, Enum):
+    NOMINAL = 'nominal'
+    ORDINAL = 'ordinal'
+    
